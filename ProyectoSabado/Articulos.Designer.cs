56277ae5d7143d4244ead7b48b2e72b8d8878eb8
VIVIAN ILEANA HERRERA ARMENTA <a214200064@unison.mx>
2021-02-12 14:44:15 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoSabado/Articulos.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace ProyectoSabado
+{
+    partial class Articulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Articulos));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnNuevo = new System.Windows.Forms.ToolStripButton();
+            this.btnEditar = new System.Windows.Forms.ToolStripButton();
+            this.btnGuardar = new System.Windows.Forms.ToolStripButton();
+            this.btnCancelar = new System.Windows.Forms.ToolStripButton();
+            this.btnSalir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.dgbArticulos = new System.Windows.Forms.DataGridView();
+            this.artID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artExistencia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artDepartamento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artPrecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtExistencia = new System.Windows.Forms.NumericUpDown();
+            this.cbDepartamento = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgbArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtExistencia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNuevo,
+            this.btnEditar,
+            this.toolStripSeparator1,
+            this.btnGuardar,
+            this.btnCancelar,
+            this.btnSalir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(765, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Image = ((System.Drawing.Image)(resources.GetObject("btnNuevo.Image")));
+            this.btnNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(76, 24);
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnEditar.Image")));
+            this.btnEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(72, 24);
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Image = ((System.Drawing.Image)(resources.GetObject("btnGuardar.Image")));
+            this.btnGuardar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(86, 24);
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Enabled = false;
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(90, 24);
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(62, 24);
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // dgbArticulos
+            // 
+            this.dgbArticulos.AllowUserToAddRows = false;
+            this.dgbArticulos.AllowUserToDeleteRows = false;
+            this.dgbArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgbArticulos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.artID,
+            this.artNombre,
+            this.artExistencia,
+            this.artDepartamento,
+            this.artPrecio});
+            this.dgbArticulos.Location = new System.Drawing.Point(12, 124);
+            this.dgbArticulos.MultiSelect = false;
+            this.dgbArticulos.Name = "dgbArticulos";
+            this.dgbArticulos.ReadOnly = true;
+            this.dgbArticulos.RowHeadersVisible = false;
+            this.dgbArticulos.RowTemplate.Height = 24;
+            this.dgbArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgbArticulos.Size = new System.Drawing.Size(741, 332);
+            this.dgbArticulos.TabIndex = 1;
+            this.dgbArticulos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dgbArticulos.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgbArticulos_CellMouseClick);
+            this.dgbArticulos.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dgbArticulos_MouseClick);
+            // 
+            // artID
+            // 
+            this.artID.HeaderText = "ID";
+            this.artID.Name = "artID";
+            this.artID.ReadOnly = true;
+            // 
+            // artNombre
+            // 
+            this.artNombre.HeaderText = "Nombre";
+            this.artNombre.Name = "artNombre";
+            this.artNombre.ReadOnly = true;
+            // 
+            // artExistencia
+            // 
+            this.artExistencia.HeaderText = "Existencia";
+            this.artExistencia.Name = "artExistencia";
+            this.artExistencia.ReadOnly = true;
+            // 
+            // artDepartamento
+            // 
+            this.artDepartamento.HeaderText = "Departamento";
+            this.artDepartamento.Name = "artDepartamento";
+            this.artDepartamento.ReadOnly = true;
+            // 
+            // artPrecio
+            // 
+            dataGridViewCellStyle9.Format = "C2";
+            dataGridViewCellStyle9.NullValue = "0";
+            this.artPrecio.DefaultCellStyle = dataGridViewCellStyle9;
+            this.artPrecio.HeaderText = "Precio";
+            this.artPrecio.Name = "artPrecio";
+            this.artPrecio.ReadOnly = true;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(63, 80);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Enabled = false;
+            this.txtPrecio.Location = new System.Drawing.Point(543, 80);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 22);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // txtExistencia
+            // 
+            this.txtExistencia.Enabled = false;
+            this.txtExistencia.Location = new System.Drawing.Point(213, 80);
+            this.txtExistencia.Name = "txtExistencia";
+            this.txtExistencia.Size = new System.Drawing.Size(120, 22);
+            this.txtExistencia.TabIndex = 4;
+            // 
+            // cbDepartamento
+            // 
+            this.cbDepartamento.Enabled = false;
+            this.cbDepartamento.FormattingEnabled = true;
+            this.cbDepartamento.Items.AddRange(new object[] {
+            "Carnes",
+            "Electronica",
+            "Farmacia",
+            "Fruteria",
+            "Hogar",
+            "Jugueteria",
+            "Limpieza"});
+            this.cbDepartamento.Location = new System.Drawing.Point(382, 78);
+            this.cbDepartamento.Name = "cbDepartamento";
+            this.cbDepartamento.Size = new System.Drawing.Size(121, 24);
+            this.cbDepartamento.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(540, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Precio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Existencia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(379, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Departamento";
+            // 
+            // Articulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 482);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbDepartamento);
+            this.Controls.Add(this.txtExistencia);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dgbArticulos);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Articulos";
+            this.Text = "Articulos";
+            this.Load += new System.EventHandler(this.Articulos_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgbArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtExistencia)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnNuevo;
+        private System.Windows.Forms.ToolStripButton btnEditar;
+        private System.Windows.Forms.ToolStripButton btnGuardar;
+        private System.Windows.Forms.ToolStripButton btnCancelar;
+        private System.Windows.Forms.ToolStripButton btnSalir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.DataGridView dgbArticulos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artExistencia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artDepartamento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artPrecio;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.NumericUpDown txtExistencia;
+        private System.Windows.Forms.ComboBox cbDepartamento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
